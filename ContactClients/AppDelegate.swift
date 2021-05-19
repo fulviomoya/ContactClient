d75d7346f44b6baa7586b4d2727b3f754055f2f3
Fulvio A. Moya <fulvio.127@gmail.com>
2021-05-19 16:09:44 -0400

    chore: adding support to older iOS versions 12 and below

--- ContactClients/AppDelegate.swift
+++ ContactClients/AppDelegate.swift
@@ -9,13 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if #available(iOS 13.0, *) {
+            return true
+        } else {
+            window?.rootViewController = UINavigationController(rootViewController: ContactListView())
+            window?.makeKeyAndVisible()
+            return true
+        }
+
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
